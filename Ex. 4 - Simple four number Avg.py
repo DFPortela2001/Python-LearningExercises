3fb517ccdbce0a7640d98078443e2ebca540eff1
Tubizonte <daniel.fonseca.portela@gmail.com>
2023-12-04 21:46:54 +0000

    Added a new excercise

---
+++ Ex. 4 - Simple four number Avg.py
@@ -0,0 +1,10 @@
+
+num1 = int(input("Please input the first grade of the student from 0-20: "))
+num2= int(input("Please second the first grade of the student from 0-20: "))
+num3 = int(input("Please third the first grade of the student from 0-20: "))
+num4 = int(input("Please forth the first grade of the student from 0-20: "))
+
+
+avg = (num1 + num2 + num3 + num4) / 4
+print("Calculating the average... The average is: ", avg)
+
