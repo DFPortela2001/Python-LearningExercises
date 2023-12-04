6159c98ad969112e3a7cea80e4d66aaa50c4f5d1
Tubizonte <daniel.fonseca.portela@gmail.com>
2023-12-04 20:58:04 +0000

    Added a new exercise

---
+++ Ex. 3 - Simple Sum.py
@@ -0,0 +1,10 @@
+import math
+
+#This only works when I declare that the variable X and Y are int
+#Só funciona quando o X e o Y são declarados como inteiros, se não o resultado vai ser 44
+
+x = int(input("Input the first number: "))
+y = int(input("Input the second number: "))
+
+
+print("The sum of the two numbers is:", x+y)
